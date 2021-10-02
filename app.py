cdc12e2aeb7646d33c39ebd36a83fa3b42039fc8
lllikov <lllllikooooov@gmail.com>
2021-10-03 00:59:52 +0300

    first steps - complete

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from sc import Soundcloud
 
-import asyncio, json_config, logging, re
+import asyncio, json_config, logging, re, os
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InputFile
 logging.basicConfig(level=logging.INFO)
@@ -15,22 +15,38 @@ dp = Dispatcher(bot)
 async def send_music(message: types.Message):
     command = re.search(r"\/\w*", message.text)
     from_user = message['from']['id']
-    if from_user == "2043806344":
-        attr_1 = re.split(r"\/\w*", message.text)
+    if from_user == 2043806344:
+        attr_1 = re.split(r"\/\w* ", message.text)
         attr_2 = attr_1[1].split()
         href = attr_2[0]
-    
+        
         if command.group(0) == "/track":
-            await sc.getTrack(href)
-
-
+            m = await bot.send_message(message.chat.id, "😌 Ищу трек...")
+            f = await sc.getTrack(href)
+            if f != 0:
+                await bot.edit_message_text('🤓 Нашел, сохраняю...', message.chat.id, m.message_id)
+                with open(f, 'rb') as fp:
+                    await bot.edit_message_text('🥳 Держи!!', message.chat.id, m.message_id)
+                    await bot.send_document(message.chat.id, fp)
+                    fp.close()
+                os.remove(f)
+            if f == 0: 
+                await bot.edit_message_text('😢 Проблемки с запросом...', message.chat.id, m.message_id)
+        
         if command.group(0) == "/playlist":
-            await sc.getPlaylist(href)
-    
-
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#     await message.answer(message.text)
+            m = await bot.send_message(message.chat.id, "😌 Ищу плейлист...")
+            fs = await sc.getPlaylist(href)
+            if fs != 0:
+                await bot.edit_message_text('🤓 Нашел, сохраняю...', message.chat.id, m.message_id)
+                for filename in fs:
+                    with open(filename, 'rb') as fp:
+                        await bot.send_document(message.chat.id, fp)
+                        asyncio.sleep(2)
+                        fp.close()
+                    os.remove(filename)
+                m = await bot.send_message(message.chat.id, "🤯 Это все. Приятного прослушивания")
+            if fs == 0:
+                await bot.edit_message_text('😢 Проблемки с запросом...', message.chat.id, m.message_id)
 
 
 if __name__ == '__main__':
