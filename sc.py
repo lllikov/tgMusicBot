9726d1a2c078ef21906f45559ab3616b55149b24
lllikov <lllllikooooov@gmail.com>
2021-10-03 00:31:31 +0300

    soundcloud class

---
+++ sc.py
@@ -0,0 +1,32 @@
+import json_config
+from sclib.asyncio import SoundcloudAPI, Track, Playlist
+
+config = json_config.connect('config.json')
+
+class Soundcloud: 
+    def __init__(self):
+        self.api = SoundcloudAPI(config['sc_client_id'])
+
+
+    async def getTrack(self, t_link):
+        print(f'_. downloading track starting now.')
+        track = await self.api.resolve(t_link)
+        assert type(track) is Track
+        filename = f'{track.artist} - {track.title}.mp3'
+        print(f'-. {filename} downloaded! saving to file.')
+        with open(filename, "wb+") as fp:
+            await track.write_mp3_to(fp)
+        return filename
+
+    async def getPlaylist(self, p_link):
+        print(f'-. downloading playlist starting now.')
+        playlist = await self.api.resolve(p_link)
+        filenames = []
+        assert type(playlist) is Playlist
+        for track in playlist.tracks:
+            filename = f'{track.artist} - {track.title}.mp3'
+            filenames.append(filename)
+            print(f'_. {filename} downloaded! saving to file.')
+            with open(filename, "wb+") as fp:
+                await track.write_mp3_to(fp)
+        return filenames
