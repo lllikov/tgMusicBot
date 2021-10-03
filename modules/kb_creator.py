0e697b362e12ec03aeed17e4f897975d74b1f569
lllikov <lllllikooooov@gmail.com>
2021-10-03 18:04:56 +0300

    full stack

--- modules/kb_creator.py
+++ modules/kb_creator.py
@@ -8,7 +8,10 @@ class KeyboardCreator():
 
     def track_create(self, json_array: list):
         k = 1
-        buttons = []
+
+        keyboard = InlineKeyboardMarkup(row_width=5)
+        next_button = InlineKeyboardButton("next", callback_data='next_page')
+        keyboard.add(next_button)
         text = ""
         for item in json_array:
             username = item['username']
@@ -16,25 +19,32 @@ class KeyboardCreator():
             uri = item['uri']
             text += f"{k}. {username} - {title}\n"
             splitted_uri = uri.split("https://api.soundcloud.com/")
-            buttons.append(
-                InlineKeyboardButton(k, callback_data=splitted_uri[1])
-            )
+            but = InlineKeyboardButton(k, callback_data = splitted_uri[1])
+            keyboard.insert(but)
             k += 1
-        return text, buttons
+        # keyboard = json.dumps([ob.__dict__ for ob in buttons])
+        # keyboard.add()
+        
+
+            
+
+        return text, keyboard
 
 
     def playlist_create(self, json_array: list):
         k = 1
-        buttons = []
         text = ""
+        keyboard = InlineKeyboardMarkup(row_width=5)
+        next_button = InlineKeyboardButton("next", callback_data='next_page')
+        keyboard.add(next_button)
         for item in json_array:
             username = item['username']
             title = item['title']
             uri = item['uri']
             track_count = item['track_count']
-            text += f"{k}. {username} - {title}. Количество треков: {track_count}"
+            text += f"{k}. {username} - {title}.    Количество треков: {track_count}\n"
             splitted_uri = uri.split("https://api.soundcloud.com/")
-            buttons.append(
-                InlineKeyboardButton(k, callback_data=splitted_uri[1])
-            )
-        return text, buttons
+            but = InlineKeyboardButton(k, callback_data = splitted_uri[1])
+            keyboard.insert(but)
+            k += 1
+        return text, keyboard
