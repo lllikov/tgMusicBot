e61470bb708700792f453f224423860e0b01b993
lllikov <lllllikooooov@gmail.com>
2021-10-03 10:53:46 +0300

    start working

--- soundcloud_searcher.py
+++ soundcloud_searcher.py
@@ -1,25 +1,36 @@
-import requests
-import soundcloud
 
-import urllib3
-import json
+from sc import Soundcloud
+import json, json_config, asyncio, requests
 from urllib.parse import quote
-client = soundcloud.Client(client_id="Z6ygXZr52uoKmSfsaK4ZpxFx1sAvSZ3n")
+
+
+
+sc = Soundcloud()
+config = json_config.connect('config.json')
 text = quote('дора')
 
-href = f'https://api-v2.soundcloud.com/search?q=%D0%B4%D0%BE%D1%80%D0%B0&sc_a_id=4cdec31472c65cff3d0b41094074719b362c5537&variant_ids=2382%2C2375&facet=model&user_id=568482-737679-121123-876021&client_id=Z6ygXZr52uoKmSfsaK4ZpxFx1sAvSZ3n&limit=20&offset=0&linked_partitioning=1&app_version=1633082668&app_locale=en'
+href = f'https://api-v2.soundcloud.com/search?q={text}&variant_ids=2382%2C2375&facet=model&client_id={config["sc_client_id"]}&limit={limit}&offset={offset}&linked_partitioning=1&app_version=1633082668&app_locale=en'
 
-# headers = {
-#     "accept": "application/json; charset=utf-8",
-#     "Authorization": "2-292926-1031896816-T6q4ZDYLTYOFj"
-# }
+
 
 r = requests.get(href)
 
-response = json.loads(r.text)
-for item in response['collection']:
-    if item['kind'] == "track":
-        text = item['uri'].split('https://api.soundcloud.com')
-        app = client.get(text[1])
-        print(app)
 
+
+
+
+
+# response = json.loads(r.text)
+# array = []
+# for item in response['collection']:
+#     if item['kind'] == "track":
+#         text = item['uri']
+#         array.append(text)
+
+
+# async def sss():
+#     await sc.getTrack(array[1])
+
+
+# if __name__ == "__main__":
+#     asyncio.run(sss())
