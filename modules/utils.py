91cc4719573dca9769102114e3b21290585cafae
lllikov <lllllikooooov@gmail.com>
2021-10-07 14:58:51 +0300

    dont remember

--- modules/utils.py
+++ modules/utils.py
@@ -17,10 +17,10 @@ class BotStates(Helper):
     PLAYLIST_STATE = ListItem()
     TRACK_DL_STATE = ListItem()
     PLAYLIST_DL_STATE = ListItem()
-    DECODE_STATE = ListItem()
+    SUB_STATE = ListItem()
     
 print(BotStates.all())
-# ['decode_state', 'playlist_dl_state', 'playlist_state', 'search_state', 'track_dl_state', 'track_state']
+# ['playlist_dl_state', 'playlist_state', 'search_state', 'sub_state', 'track_dl_state', 'track_state']
 
 headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
